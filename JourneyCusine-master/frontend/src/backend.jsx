eac97ea18c7bbb20842911582f631588a59d3b37
Jyoti07-git <144542779+Jyoti07-git@users.noreply.github.com>
2024-08-05 21:39:02 +0530

    Update backend.jsx

--- JourneyCusine-master/frontend/src/backend.jsx
+++ JourneyCusine-master/frontend/src/backend.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 // export const API = "http://localhost:5001/";
-export const API = "https://backend-api-jc-production.up.railway.app/";
+export const API = "https://hotel-application-backend.onrender.com";
 
 const api = axios.create({
   // baseURL: "http://localhost:5001/",
-  baseURL: "https://backend-api-jc-production.up.railway.app/",
+  baseURL: "https://hotel-application-backend.onrender.com",
 });
 
 api.interceptors.request.use(
