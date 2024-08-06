08fe3998cf78bcdac47ba097e8cd14e3a744255d
jyotiprava <alinapanda63@gmail.com>
2024-08-06 13:02:28 +0530

    changes in store

--- JourneyCusine-master/frontend/src/redux/store.js
+++ JourneyCusine-master/frontend/src/redux/store.js
@@ -1,9 +1,9 @@
-// src/redux/store.js
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers/rootReducer";
+import rootReducer from "./reducers/rootReducer"; // Adjust the path if necessary
 
+const logger = createLogger();
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
--- JourneyCusine-master/frontend/vite.config.js
+++ JourneyCusine-master/frontend/vite.config.js
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-  },
+  plugins: [
+    react(),
+    nodeResolve({
+      browser: true,
+      extensions: ['.js', '.jsx', '.json']
+    }),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'), // or 'production'
+      preventAssignment: true,
+    })
+  ],
   build: {
     rollupOptions: {
-      external: ['@stripe/stripe-js'], // Add external modules if needed
-    },
-  },
+      external: ['redux', 'redux-logger', 'redux-thunk', 'react-redux']
+    }
+  }
 });
